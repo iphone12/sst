c24625684321096f1348e2c10cca371f7d177cb0
ZXH <MAC@www.gd.10086.cn>
2014-01-14 10:02:35 +0800

    Initial Commit

---
+++ MyMusicPlayer/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MyMusicPlayer
+//
+//  Created by ZXH on 14-1-14.
+//  Copyright (c) 2014年 ZXh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
